7eda755b5f23e7279c57affa856e2550261f69f6
GauravBoruah <boruahborajen2019@gmail.com>
2026-07-24 19:57:53 +0530

    Update most_successful_venue.py

--- problems/most_successful_venue.py
+++ problems/most_successful_venue.py
@@ -6,11 +6,11 @@ df = pd.read_csv("../data/cleaned_data.csv")
 
 def most_successful_venue(df):
 
-    venue_wins = df[df["Match_Result"] == "Win"].groupby("Venue")["Match_ID"].nunique()
+    wins = df[df["Match_Result"] == "Win"].groupby("Venue")["Match_ID"].nunique()
 
-    venue_wins = venue_wins.sort_values(ascending=False)
+    wins = wins.sort_values(ascending=False)
 
-    return venue_wins
+    return wins
 
 print("Most Successful Venues:")
 
